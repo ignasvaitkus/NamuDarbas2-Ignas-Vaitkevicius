f889d61a4558f90fdcb8f9ba31692c71bdd673c4
ignasvait <vaitkusignas@gmail.com>
2020-01-12 17:07:59 +0200

    Pradeti pagrindai

--- NamuDarbas2/NamuDarbas2/Program.cs
+++ NamuDarbas2/NamuDarbas2/Program.cs
@@ -6,7 +6,25 @@ namespace NamuDarbas2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] magicNumber= new int[5];
+             InsertNumbers(magicNumber);
+            PrintNumbers(magicNumber);
+        }
+        
+        static void InsertNumbers( int[] skaiciai)
+        {
+            for(int i=0;i<skaiciai.Length;i++)
+            {
+               skaiciai[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        static void PrintNumbers(int [] skaiciai)
+        {
+            foreach (int item in skaiciai)
+            {
+                Console.Write(item + ", ");
+            }
         }
     }
 }
