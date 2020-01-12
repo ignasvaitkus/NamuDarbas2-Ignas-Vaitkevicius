dd5e527829036eb4a3f46d434caf843ccf3289f3
ignasvait <vaitkusignas@gmail.com>
2020-01-12 17:55:24 +0200

    Programa patikrina ar ivesti skaiciai nesikartoja

--- NamuDarbas2/NamuDarbas2/Program.cs
+++ NamuDarbas2/NamuDarbas2/Program.cs
@@ -6,9 +6,14 @@ namespace NamuDarbas2
     {
         static void Main(string[] args)
         {
-            int[] magicNumber= new int[5];
+            int[] magicNumber= new int[6];
              InsertNumbers(magicNumber);
             PrintNumbers(magicNumber);
+            Console.WriteLine();
+            if (!CheckNumbers(magicNumber))
+            { Console.WriteLine("Klaida, yra pasikartojantys skaiciai"); return; }
+            else Console.WriteLine("Visi skaiciai skirtingi");
+
         }
         
         static void InsertNumbers( int[] skaiciai)
@@ -21,10 +26,31 @@ namespace NamuDarbas2
 
         static void PrintNumbers(int [] skaiciai)
         {
-            foreach (int item in skaiciai)
+            foreach (int number in skaiciai)
             {
-                Console.Write(item + ", ");
+                Console.Write(number);
             }
+        }
+
+        static bool CheckNumbers(int [] skaiciai)
+        {
+            bool check=true;
+            for(int i=0;i<skaiciai.Length;i++)
+            {
+                int skc1 = skaiciai[i];
+                for(int j=0;j<skaiciai.Length;j++)
+                {
+                    int skc2 = -1;
+                    if(i!=j) skc2 = skaiciai[j];
+                    if(skc1==skc2)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+
+            }
+            return check;
         }
     }
 }
