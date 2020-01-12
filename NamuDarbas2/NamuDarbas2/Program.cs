f463cc5006da17dd7a6d49e3665cf38e3c4f8cbb
ignasvait <vaitkusignas@gmail.com>
2020-01-13 00:07:38 +0200

    Programa sugeba padauginti masyva is 2 ir patikrinti ar jis netampa 7zenklis

--- NamuDarbas2/NamuDarbas2/Program.cs
+++ NamuDarbas2/NamuDarbas2/Program.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 
+
 namespace NamuDarbas2
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        { Start:
             int[] magicNumber= new int[6];
              InsertNumbers(magicNumber);
             PrintNumbers(magicNumber);
             Console.WriteLine();
             if (!CheckNumbers(magicNumber))
-            { Console.WriteLine("Klaida, yra pasikartojantys skaiciai"); return; }
+            { Console.WriteLine("Klaida, yra pasikartojantys skaiciai, bandykite is naujo"); goto Start;   }
             else Console.WriteLine("Visi skaiciai skirtingi");
+
+            if (MakeNewInt(magicNumber) > 999999)
+            {
+                Console.WriteLine("Magiskas skaicius netinkamas, bandykite is naujo!");
+                goto Start;
+            }
+            else
+            {
+                int skaicius = MakeNewInt(magicNumber);
+                MakeNewArray(skaicius, magicNumber);
+            }
+            PrintNumbers(magicNumber);
+            Console.WriteLine();
+            if (CheckSize(magicNumber)) Console.WriteLine("Skaicius telpa i ribas!");
+            else Console.WriteLine("Skaicius netelpa i ribas!");
 
         }
         
@@ -51,6 +67,46 @@ namespace NamuDarbas2
 
             }
             return check;
+        }
+
+        static bool CheckSize(int[] skaicius)
+        {
+            bool check = true;
+            int sum = 0;
+            int x = 100000;
+            for (int i=0;i<skaicius.Length;i++)
+            {
+                sum = sum + skaicius[i]*x;
+                x /= 10;
+            }
+            if (sum > 999999) check = false;
+            return check;
+        }
+
+        static int MakeNewInt(int[] skaicius)
+        {
+
+            int sum = 0;
+            int x = 100000;
+            for (int i = 0; i < skaicius.Length; i++)
+            {
+                sum = sum + skaicius[i] * x;
+                x /= 10;
+            }
+            sum *= 2;
+            return sum;
+            
+        }
+        static void MakeNewArray(int sum, int[] skaicius)
+        {
+            for (int i = skaicius.Length - 1; i >= 0; i--)
+            {
+                int skc = sum % 10;
+                skaicius[i] = skc;
+                sum /= 10;
+
+            }
+        
         }
     }
 }
